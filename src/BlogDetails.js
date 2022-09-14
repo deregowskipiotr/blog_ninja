09e89d9de5c424cd35067d109e5af1225128736d
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-09-14 18:32:21 +0200

    Finished ...

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,32 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import useFetch from './useFetch';
+
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:3000/blogs/' + id);
+  const history = useNavigate();
+
+  const handleClick = () => {
+    fetch('http://localhost:3000/blogs/' + blog.id, {
+      method: 'DELETE',
+    }).then(() => {
+      history('/');
+    })
+  }
 
   return (
     <div className="blog-details">
-      <h2>Blog details - { id }</h2>
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+          <button onClick={ handleClick }>Delete</button>
+        </article>
+      )}
     </div>
   );
 }
--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return ( 
     <div className="not-found">
       <h2>Error 404 not found</h2>
+      <Link to="/">Back to the home page...</Link>
     </div>
    );
 }
